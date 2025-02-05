e2309af244abdd924b0f169a8a3d6f5eb0862605
Abdelrahman Amr <abdelrahmanamrxi@gmail.com>
2025-02-05 18:39:00 +0200

    Cross Origin Route Fixed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(CookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:process.env.FRONTEND_ROUTE,
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
     credentials:true
 }))
